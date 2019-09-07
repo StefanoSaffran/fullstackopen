c5c4292632568264071f3450cb7e41361800a95a
Stefano Akira Saffran <stefanoas@gmail.com>
2019-09-07 23:12:48 +0200

    adding part1 - exer 1.2 course information, step2

--- part 1/part1/src/index.js
+++ part 1/part1/src/index.js
@@ -12,16 +12,18 @@ const Header = props => {
 const Content = props => {
     return (
         <div>
-            <p>
-                {props.part1} {props.exercises1}
-            </p>
-            <p>
-                {props.part2} {props.exercises2}
-            </p>
-            <p>
-                {props.part3} {props.exercises3}
-            </p>
+            <Part part={props.part1} exercises={props.exercises1} />
+            <Part part={props.part2} exercises={props.exercises2} />
+            <Part part={props.part3} exercises={props.exercises3} />
         </div>
+    )
+}
+
+const Part = props => {
+    return (
+        <p>
+            {props.part} {props.exercises}
+        </p>
     )
 }
 
@@ -46,9 +48,7 @@ const App = () => {
     return (
         <div>
             <Header course={course} />
-
             <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-
             <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
         </div>
     )
