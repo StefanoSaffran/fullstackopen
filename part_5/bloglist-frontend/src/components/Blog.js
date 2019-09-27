d89c670616c4eeac2079a999aee37953c94134f6
Stefano Akira Saffran <stefanoas@gmail.com>
2019-09-27 19:21:05 +0200

    adding part 5 - exer 5.6* bloglist frontend step6

--- part_5/bloglist-frontend/src/components/Blog.js
+++ part_5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
 
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={toggleVisibility} style={hideWhenVisible}>
+        {blog.title} {blog.author}
+      </div>
+      <div style={showWhenVisible} onClick={toggleVisibility}>
+        {blog.title} {blog.author} <br/>
+        {blog.url} <br/>
+        {blog.likes} likes <button>like</button> <br/>
+        added by {blog.author}
+      </div>
+    </div>
+  )
+}
 export default Blog
