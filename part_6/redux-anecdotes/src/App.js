da0d12690816c1567657092f197eda9b23da7b01
Stefano Akira Saffran <stefanoas@gmail.com>
2019-10-04 22:40:26 +0200

    adding part 6 - exer 6.3 anecdotes step1

---
+++ part_6/redux-anecdotes/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { addVote } from './reducers/anecdoteReducer';
+
+const App = (props) => {
+  const anecdotes = props.store.getState();
+
+  const vote = (id) => {
+    props.store.dispatch(
+      addVote(id)
+    );
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form>
+        <div><input /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
