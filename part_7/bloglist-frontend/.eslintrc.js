44ace80a2fff0f87cb5378f23417a440e50f9f83
Stefano Saffran <stefanosaffran@hotmail.com>
2020-01-07 16:20:22 +0100

    feat: add eslint/prettier/editorconfig

--- part_7/bloglist-frontend/.eslintrc.js
+++ part_7/bloglist-frontend/.eslintrc.js
@@ -1,43 +1,60 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "jest/globals": true
+    env: {
+      browser: true,
+      es6: true,
+      jest: true,
     },
-    "extends": [ 
-        "eslint:recommended",
-        "plugin:react/recommended"
+    extends: [
+      'airbnb',
+      'prettier',
+      'prettier/react'
     ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+      __DEV__: 'readonly',
+    },
+    parser: 'babel-eslint',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+      'prettier',
+      'react-hooks'
+    ],
+    rules: {
+      'prettier/prettier': 'error',
+      'react/jsx-filename-extension': [
+        'warn',
+        { extensions: ['.jsx', '.js' ] }
+      ],
+      'import/prefer-default-export': 'off',
+      "react/state-in-constructor": "off",
+          "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      'react/static-property-placement':'off',
+      'react/jsx-props-no-spreading': 'off',
+          "react/jsx-one-expression-per-line": "off",
+      "global-require": "off",
+      "react-native/no-raw-text": "off",
+      'no-param-reassign': 'off',
+          "no-underscore-dangle": "off",
+      camelcase: "off",
+      'no-console': ['error', { allow: ['tron'] }],
+  
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+    settings: {
+      "import/resolver": {
+        "babel-plugin-root-import": {
+          rootPathSuffix: "src"
         },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+      },
     },
-    "plugins": [
-        "react", "jest"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": 0,
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": 0,
-        "eqeqeq": "error",
-        "no-trailing-spaces": "error",
-        "object-curly-spacing": [
-            "error", "always"
-        ],
-        "arrow-spacing": [
-            "error", { "before": true, "after": true }
-        ],
-        "no-console": 0,
-        "react/prop-types": 0
-    }
   };
+  
