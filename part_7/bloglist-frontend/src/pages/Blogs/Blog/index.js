a7dbcda8144d9197a13173d541a72b81eea1c9c7
Stefano Saffran <stefanosaffran@hotmail.com>
2020-01-08 21:49:16 +0100

    feat: add comments feature

--- part_7/bloglist-frontend/src/pages/Blogs/Blog/index.js
+++ part_7/bloglist-frontend/src/pages/Blogs/Blog/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 import { toast } from 'react-toastify';
 
@@ -8,12 +8,13 @@ import Loading from '~/components/Loading';
 import api from '~/services/api';
 import history from '~/services/history';
 
-import { Container } from './styles';
+import { Container, Comments, Form } from './styles';
 
 export default function Blog() {
   const user = useSelector(state => state.user.profile);
   const { id } = useParams();
   const [blog, setBlog] = useState([]);
+  const [comment, setComment] = useState('');
   const [loading, setLoading] = useState(true);
 
   const loadBlog = async () => {
@@ -36,6 +37,7 @@ export default function Blog() {
 
   const handleLikes = async () => {
     const blogToUpdate = {
+      comments: blog.comments.map(c => c.id),
       user: blog.user.id,
       likes: (blog.likes += 1),
       author: blog.author,
@@ -53,6 +55,11 @@ export default function Blog() {
           name: blog.user.name,
           id: blog.user.id,
         },
+        comments: blog.comments.map(c => ({
+          text: c.text,
+          id: c.id,
+          blog: c.blog,
+        })),
       });
       toast.success(`${user.name} liked the blog post ${data.title}`);
     } catch (err) {
@@ -82,29 +89,86 @@ export default function Blog() {
       }
     }
   };
+
+  const handleComment = async e => {
+    e.preventDefault();
+    try {
+      const { data } = await api.post(`/api/blogs/${id}/comments`, {
+        text: comment,
+      });
+
+      setBlog({
+        ...blog,
+        comments: blog.comments.concat(data),
+      });
+      setComment('');
+    } catch (err) {
+      toast.error(
+        (err.response && err.response.data.error) ||
+          'Error trying to add a comment'
+      );
+    }
+  };
+
   return (
     <Container>
       {loading ? (
         <Loading type="spinner" />
       ) : (
-        <div>
-          <h1>
-            {blog.title} {blog.author} <br />
-          </h1>
-          <a href={blog.url}>{blog.url}</a> <br />
+        <>
           <div>
-            <p>{blog.likes} likes</p>
-            <button type="button" onClick={() => handleLikes(blog)}>
-              like
-            </button>
+            <h1>
+              {blog.title} {blog.author} <br />
+            </h1>
+            <a className="dont-break-out" href={blog.url}>
+              {blog.url}
+            </a>{' '}
+            <br />
+            <div>
+              <p>{blog.likes} likes</p>
+              <button
+                className="like"
+                type="button"
+                onClick={() => handleLikes(blog)}
+              >
+                like
+              </button>
+            </div>
+            <p>added by {blog.user.name || user.name} </p>
+            {blog.user.name === user.name && (
+              <button
+                type="button"
+                className="remove"
+                onClick={() => handleDelete(blog)}
+              >
+                remove
+              </button>
+            )}
           </div>
-          added by {blog.user.name || user.name} <br />
-          {blog.user.name === user.name && (
-            <button type="button" onClick={() => handleDelete(blog)}>
-              remove
-            </button>
-          )}
-        </div>
+          <Comments>
+            <h3>comments</h3>
+            <Form onSubmit={handleComment}>
+              <div>
+                <input
+                  type="text"
+                  value={comment}
+                  onChange={({ target }) => {
+                    setComment(target.value);
+                  }}
+                />
+
+                <button type="submit" className="addComment">
+                  add comment
+                </button>
+              </div>
+            </Form>
+            <ul>
+              {blog.comments &&
+                blog.comments.map(c => <li key={c.id}>{c.text}</li>)}
+            </ul>
+          </Comments>
+          <Link to="/blogs">Back to Blogs</Link>
+        </>
       )}
     </Container>
   );
--- part_7/bloglist-frontend/src/pages/Blogs/Blog/styles.js
+++ part_7/bloglist-frontend/src/pages/Blogs/Blog/styles.js
@@ -17,23 +17,132 @@ export const Container = styled.div`
     margin-bottom: 10px;
   }
 
-  a {
-    color: ${colors.primary};
-    margin: 10px 0;
-    font-size: 16px;
-    &:hover {
-      color: ${darken(0.08, `${colors.primary}`)};
-    }
-  }
-
   div {
+    a.dont-break-out {
+      /* css to handle long urls */
+
+      /* These are technically the same, but use both */
+      overflow-wrap: break-word;
+      word-wrap: break-word;
+
+      -ms-word-break: break-all;
+      /* This is the dangerous one in WebKit, as it breaks things wherever */
+      word-break: break-all;
+      /* Instead use this non-standard one: */
+      word-break: break-word;
+
+      /* Adds a hyphen where the word breaks, if supported (No Blink) */
+      -ms-hyphens: auto;
+      -moz-hyphens: auto;
+      -webkit-hyphens: auto;
+      hyphens: auto;
+
+      margin: 10px 0;
+      font-size: 16px;
+    }
+
+    button.remove {
+      height: 24px;
+      background: #ee4d64;
+      border: 0;
+      border-radius: 4px;
+      font-weight: bold;
+      color: ${colors.white};
+      margin: 10px 0 5px;
+      font-size: 12px;
+      padding: 0 10px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.08, '#ee4d64')};
+      }
+    }
+
     div {
       display: flex;
       flex-direction: row;
       align-items: center;
 
+      button.like {
+        height: 20px;
+        background: ${colors.addButton};
+        transition: background 0.2s;
+        margin: 5px 0;
+        border: 0;
+        border-radius: 4px;
+        font-weight: bold;
+        color: ${colors.white};
+        font-size: 12px;
+        padding: 0 10px;
+
+        &:hover {
+          background: ${darken(0.08, `${colors.addButton}`)};
+        }
+      }
+
       p {
         margin-right: 5px;
+      }
+    }
+  }
+
+  a {
+    text-align: center;
+    margin-top: 20px;
+    color: ${colors.addButton};
+    font-size: 16px;
+
+    &:hover {
+      color: ${darken(0.08, `${colors.addButton}`)};
+    }
+  }
+`;
+
+export const Comments = styled.div`
+  margin-top: 20px;
+
+  ul {
+    list-style-type: square;
+    margin-top: 15px;
+    margin-left: 25px;
+
+    li {
+      line-height: 1.5;
+    }
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 15px;
+
+  div {
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    align-items: center;
+
+    input {
+      width: 50%;
+      height: 28px;
+      padding: 0 15px;
+      border-radius: 4px;
+      border: 1px solid ${colors.border};
+      margin-right: 10px;
+    }
+
+    button {
+      height: 28px;
+      background: ${colors.addButton};
+      border: 0;
+      border-radius: 4px;
+      font-weight: bold;
+      color: ${colors.white};
+      font-size: 12px;
+      padding: 0 10px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.08, `${colors.addButton}`)};
       }
     }
   }
