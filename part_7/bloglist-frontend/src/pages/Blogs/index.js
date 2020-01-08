a21cd687e4fe760f254178c6b86dec6bf87127d7
Stefano Saffran <stefanosaffran@hotmail.com>
2020-01-08 21:52:08 +0100

    feat: added create new blog feature

--- part_7/bloglist-frontend/src/pages/Blogs/index.js
+++ part_7/bloglist-frontend/src/pages/Blogs/index.js
@@ -3,7 +3,10 @@ import React, { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 
 import Loading from '~/components/Loading';
+import Togglable from '~/components/Togglable';
 import Blog from '~/components/BlogComponent';
+import BlogForm from '~/components/BlogForm';
+import { useField } from '~/helpers/hooks/useField';
 import api from '~/services/api';
 
 import { Container } from './styles';
@@ -11,6 +14,11 @@ import { Container } from './styles';
 export default function Blogs() {
   const [blogs, setBlogs] = useState([]);
   const [loading, setLoading] = useState(true);
+  const { reset: resetTitle, ...title } = useField('text', 'title');
+  const { reset: resetAuthor, ...author } = useField('text', 'author');
+  const { reset: resetUrl, ...url } = useField('text', 'url');
+
+  const blogFormRef = React.createRef();
 
   const loadBlogs = async () => {
     try {
@@ -30,6 +38,32 @@ export default function Blogs() {
     loadBlogs();
   }, []);
 
+  const handleBlogSubmit = async event => {
+    event.preventDefault();
+    blogFormRef.current.toggleVisibility();
+    const blogObject = {
+      title: title.value,
+      author: author.value,
+      url: url.value,
+    };
+
+    try {
+      const { data } = await api.post('/api/blogs', blogObject);
+
+      setBlogs(blogs.concat(data));
+      toast.success(`a new blog ${data.title} by ${data.author} added`);
+    } catch (err) {
+      toast.error(
+        (err.response && err.response.data.error) ||
+          'Error connecting to the server'
+      );
+    } finally {
+      resetTitle();
+      resetAuthor();
+      resetUrl();
+    }
+  };
+
   const sortByKey = (blogsToSort, key) => {
     return blogsToSort.sort((a, b) => {
       const x = b[key];
@@ -47,6 +81,15 @@ export default function Blogs() {
       ) : (
         <>
           <h1>Blogs</h1>
+          <h2>create new</h2>
+          <Togglable buttonLabel="new note" ref={blogFormRef}>
+            <BlogForm
+              handleBlogSubmit={handleBlogSubmit}
+              inputTitle={<input {...title} />}
+              inputAuthor={<input {...author} />}
+              inputUrl={<input {...url} />}
+            />
+          </Togglable>
           {sortedBlogs.map(blog => (
             <Blog key={blog.id} blog={blog} />
           ))}
